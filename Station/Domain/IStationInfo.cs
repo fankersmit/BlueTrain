e0b8682c6df44e340d89b8afa997e21aa6b30f22
frits <frits.ankersmit@norne.nl>
2018-10-04 01:13:49 +0200

    added IStationInfo

---
+++ Station/Domain/IStationInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Station.Domain
+{
+    public interface IStationInfo
+    {
+        Guid Id { get; };
+        string Name { get; };
+        string Description { get;  }
+        Dictionary<string, string> Capabilities { get;  }
+        StationStatus Status { get; set; }
+    }
+}
