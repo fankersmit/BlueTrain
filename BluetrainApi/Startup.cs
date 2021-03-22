dfe3b33345348ed34afc9e536237358d38b05966
FritsA <frits.ankersmit@norne.nl>
2021-03-22 22:43:36 +0100

    Added terminal state logic

--- BluetrainApi/Startup.cs
+++ BluetrainApi/Startup.cs
@@ -31,7 +31,7 @@ namespace Api
             services.AddControllers()
                 .AddJsonOptions(options =>
                     options.JsonSerializerOptions.PropertyNamingPolicy = null);
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "BluetTrain Api", Version = "v1"}); });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "BluetTrain BluetrainApi", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +41,7 @@ namespace Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlueTrain Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlueTrain BluetrainApi v1"));
             }
 
             app.UseHttpsRedirection();
